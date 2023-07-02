bd28385be81341bdf2ef9ba1f325a828fbecddb3
antihaddock <antihaddock1@gmail.com>
2023-07-02 11:19:58 +1000

    fixing integration test

--- integration/test_docker.py
+++ integration/test_docker.py
@@ -3,22 +3,24 @@
 import json
 import requests
 from deepdiff import DeepDiff
+import pandas as pd
 
-with open('event.json', 'rt', encoding='utf-8') as f_in:
-    event = json.load(f_in)
+# with open('event.json', 'rt', encoding='utf-8') as f_in:
+#     event = json.load(f_in)
 
 
-url = 'http://0.0.0.0:5000'
+url = 'http://0.0.0.0:5000/predict_outcome'
 
 
 df = pd.read_csv('../data/integration_test_data.csv')
 
-# This will run through and provide a prediction against each row in the integration_test_data.csv
-client = df.to_dict()
+# This will run through and provide a prediction against each row in the test_data.csv
+for i in range(len(df)):
+    client = df.iloc[i].to_dict()
 
 response = requests.post(url, json=client)
 actual_response = response.json()
-
+print(actual_response)
 expected_response = {'Class': [0], 'probability': [[0.8747006574382459, 0.12529934256175407]]}
 
 
